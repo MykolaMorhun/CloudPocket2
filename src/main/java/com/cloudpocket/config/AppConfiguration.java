bcaeec01ad998d8de4e08518b6fc1abf2129ff30
Nicholas.Morhun <Nicksbox78@gmail.com>
2016-09-24 16:17:57 +0300

    Use embedded SQLite database

--- src/main/java/com/cloudpocket/config/AppConfiguration.java
+++ src/main/java/com/cloudpocket/config/AppConfiguration.java
@@ -1,11 +1,9 @@
 package com.cloudpocket.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.cloudpocket")
@@ -14,15 +12,6 @@ public class AppConfiguration {
     @Bean
     public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public DataSource dataSource(@Value("${db.url}") String url,
-                                 @Value("${db.username}") String username,
-                                 @Value("${db.password}") String password) {
-        DriverManagerDataSource source = new DriverManagerDataSource(url, username, password);
-        source.setDriverClassName("com.mysql.jdbc.Driver");
-        return source;
     }
 
 }
---
+++ src/main/java/com/cloudpocket/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.cloudpocket.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource(@Value("${database.name}") String databaseName) {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:" + databaseName);
+        return dataSourceBuilder.build();
+    }
+
+}
